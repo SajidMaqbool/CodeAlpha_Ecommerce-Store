a26746c0a0162855f018d806d4f3d5c4cc54c8bd
SajidMaqbool <f2023065305@umt.edu.pk>
2026-08-16 22:36:07 -0700

    Initial commit - Full stack e-commerce store

---
+++ models/Product.js
@@ -0,0 +1,1 @@
+﻿const mongoose = require("mongoose"); const productSchema = new mongoose.Schema({ title: { type: String, required: true }, description: String, price: { type: Number, required: true }, image: String, stock: { type: Number, default: 10 } }); module.exports = mongoose.model("Product", productSchema);
